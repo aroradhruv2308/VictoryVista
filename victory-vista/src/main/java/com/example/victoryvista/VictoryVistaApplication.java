9e0cb7e30cb673d1be99ae94652cbb71b965fdba
dhruvarora2307 <aroradhruv2308@gmail.com>
2024-01-09 04:49:37 +0530

    Did Project Setup added auto reload and also cleared console

---
+++ victory-vista/src/main/java/com/example/victoryvista/VictoryVistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.victoryvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictoryVistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictoryVistaApplication.class, args);
+	}
+
+}
---
+++ victory-vista/src/test/java/com/example/victoryvista/VictoryVistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.victoryvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictoryVistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
